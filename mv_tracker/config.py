cbfae980fa3b6463f72ed8c17b0b7d36a9d61647
Poirion <o.poirion@gmail.com>
2015-12-23 18:11:49 +0100

    add video

--- mv_tracker/config.py
+++ mv_tracker/config.py
@@ -11,26 +11,26 @@ import numpy as np
 ctime = time()
 #################################
 
-######### paramètre de l'algo meanshift ##########
+######### paramètre de l'algo  meanshift ##########
 term_crit = ( cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 1 )
 ##################################################
 
 
 ##### Variable ###########
-tseuil = 0.5 # secondes
-kernel = np.ones((2, 2), np.uint8) # Nombre de pixel en x et en y traités par les fonctions filtre: enlève le bruit
-kernelerode = np.ones((2, 2), np.uint8) # Nombre de pixel en x et en y traités par les fonctions filtre: enlève le bruit
-erodeseuil = 150000 # Somme d'une image max au dela delaquelleest appliqué un filtre d'érosion
-nbproc = 1 # Nombre de process effectuant l'algo DBSCAN en parallèle
-eps = 50 # paramètre de DBSCAN ~distance min pour un pixel pour intégrer un cluster
-min_samples = 60 # paramètre de DBSCAN nombre de pixels min pour former un cluster
-clustermax = 25 # Nombre de clusters max en simultané sur une image
-cl_list_size = 10 # Inertie de la durée de vie d'une zone vide en mouvement
-sum_list_size = 5 # Inertie du seuil de detection d'un mouvement dans l'image
-reduce_thres = 25 # seuil (intensité) controlant le déclenchement l'érosion d'une zone de mouvement
-erode_latence = 1.0 # latence (s) avant erosion
-delete_factor = 0.3 # facteur controlant le déclenchement de la suppression d'une zone de moument
-motion_detection_thres = 18000 # seuil de detection d'un mouvement
+tseuil = 0.5 # seconds
+kernel = np.ones((2, 2), np.uint8) # Noise dealer: Number of pixel in x and y removed
+kernelerode = np.ones((2, 2), np.uint8) # Number of pixel in x and y removed for each erode iter
+erodeseuil = 150000 # erosion process threshold triggering
+nbproc = 1 # nb of clustering processes to launch in parallel
+eps = 50 # DBSCAN parameter ~min distance for a pixel to be allowed to integrate a cluster
+min_samples = 60 # DBSCAN parameter ~ min number of pixels to form a cluster
+clustermax = 25 # max number of simultaned clusters
+cl_list_size = 10 # lifetime inertia of a movement area
+sum_list_size = 5 # Inertia of the movement detection threshold
+reduce_thres = 25 # threshold (pixels sum intensity) triggering area erosion
+erode_latence = 1.0 # latence (s) before erosion
+delete_factor = 0.3 # factor controling the suppresion of a movement area
+motion_detection_thres = 18000 # threshold for movement detection
 #########################
 
 
